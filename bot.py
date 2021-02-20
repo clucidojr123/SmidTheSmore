f5e6994578683de4e93b78c0a25018eeb40b9f33
clucidojr123 <clucidojr123@gmail.com>
2021-02-19 23:49:51 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        await message.channel.send('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(TOKEN)
+
+
