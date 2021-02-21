2b0d6e3194e2d037b78d3a919e79740bdf102618
clucidojr123 <clucidojr123@gmail.com>
2021-02-21 00:40:11 -0500

    add new commands

--- commands/fun.py
+++ commands/fun.py
@@ -17,5 +17,33 @@ class Fun(commands.Cog):
         else:
             data = res.json()
             await ctx.reply(data['text'])
+
+    @commands.command(aliases=['roast', 'nice', 'comp'])
+    async def compliment(self, ctx):
+        """Returns a random compliment."""
+
+        # call API
+        res = requests.get("https://complimentr.com/api")
+
+        # Send proper response depending on request status
+        if(res.status_code != 200):
+            await ctx.reply('An error has occurred. Sorry!')
+        else:
+            data = res.json()
+            await ctx.reply(data['compliment'])
+    
+    @commands.command(aliases=['laugh', 'dadjoke'])
+    async def joke(self, ctx):
+        """Returns a random joke."""
+        
+        # call API
+        res = requests.get("https://official-joke-api.appspot.com/jokes/random")
+
+        # Send proper response depending on request status
+        if(res.status_code != 200):
+            await ctx.reply('An error has occurred. Sorry!')
+        else:
+            data = res.json()
+            await ctx.reply(data['setup'] + "\n\n" + f"||{data['punchline']}||")
     
         
